c9f2a63081decd52a3daea8e7f81ffd312fca8ec
lessw2020 <lessw@etrillium.com>
2024-11-22 13:24:14 -0800

    prep fsdp wrapping

--- fsdp_parallelize.py
+++ fsdp_parallelize.py
@@ -5,7 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 # This file applies the PT-D parallelisms (except pipeline parallelism) and various
-# training techniques (e.g. activation checkpointing and compile) to the Llama model.
+# training techniques (e.g. activation checkpointing and compile) to the NanoGPT model.
 
 from collections import defaultdict
 
@@ -23,20 +23,12 @@ from torch.distributed._tensor import Replicate, Shard
 from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
     checkpoint_wrapper as ptd_checkpoint_wrapper,
 )
-from torch.distributed.tensor.parallel import (
-    ColwiseParallel,
-    parallelize_module,
-    PrepareModuleInput,
-    RowwiseParallel,
-    SequenceParallel,
-)
 
 
 def parallelize_nanogpt(
     model: nn.Module,
     world_mesh: DeviceMesh,
     parallel_dims: ParallelDims,
-    job_config: JobConfig,
 ):
     """
     Apply tensor parallelism, activation checkpointing, torch.compile, and data
@@ -46,63 +38,61 @@ def parallelize_nanogpt(
     the model must fit on GPU or CPU memory.
     """
 
-    if job_config.activation_checkpoint.mode != "none":
-        apply_ac(model, job_config.activation_checkpoint)
+    # TODO - AC if job_config.activation_checkpoint.mode != "none":
+    # apply_ac(model, job_config.activation_checkpoint)
 
     # turn on per-TransformerBlock compile after AC wrapping and before FSDP
-    if job_config.training.compile:
+    """if job_config.training.compile:
         if job_config.model.norm_type == "fused_rmsnorm":
             raise NotImplementedError(
                 "fused_rmsnorm is not compatible with torch.compile yet. "
                 "Please use rmsnorm or layernorm."
             )
         apply_compile(model)
+    """
 
-    if (
-        parallel_dims.dp_shard_enabled
-    ):  # apply FSDP or HSDP, potentially with Context Parallel
-        try:
-            dp_mesh = (
-                world_mesh["dp_cp"] if parallel_dims.cp_enabled else world_mesh["dp"]
-            )
-        except IndexError:
-            # note: this is a workaround of the above logic for old pytorch version
-            # where https://github.com/pytorch/pytorch/pull/138945 is not included
-            # throw a warning to encourage users to upgrade to a newer pytorch version
-            check_if_feature_in_pytorch(
-                "DeviceMesh flattening over 3D+ meshes",
-                "https://github.com/pytorch/pytorch/pull/138945",
-                "2.6.0.dev20241030",
-            )
-            # TODO: remove this workaround once PyTorch 2.6 is released
-            dp_mesh_dim_names = ("dp",)
+    # apply FSDP or HSDP, potentially with Context Parallel
+    try:
+        dp_mesh = world_mesh["dp"]
+    except IndexError:
+        # note: this is a workaround of the above logic for old pytorch version
+        # where https://github.com/pytorch/pytorch/pull/138945 is not included
+        # throw a warning to encourage users to upgrade to a newer pytorch version
+        dp_mesh_dim_names = ("dp",)
 
-            # note that mesh can only be flattened from the finest-grained mesh dimensions
-            dp_mesh = world_mesh[dp_mesh_dim_names]
+        # note that mesh can only be flattened from the finest-grained mesh dimensions
+        dp_mesh = world_mesh[dp_mesh_dim_names]
 
-        apply_fsdp(
-            model,
-            dp_mesh,
-            param_dtype=TORCH_DTYPE_MAP[job_config.training.mixed_precision_param],
-            reduce_dtype=TORCH_DTYPE_MAP[job_config.training.mixed_precision_reduce],
-            tp_enabled=parallel_dims.tp_enabled,
-            pp_enabled=parallel_dims.pp_enabled,
-            cpu_offload=job_config.training.enable_cpu_offload,
+    apply_fsdp(
+        model,
+        dp_mesh,
+        param_dtype=torch.bfloat16,
+        reduce_dtype=torch.bfloat16,
+    )
+
+    print("Applied FSDP to the model")
+
+
+def apply_fsdp(
+    model: nn.Module,
+    dp_mesh: DeviceMesh,
+    param_dtype: torch.dtype,
+    reduce_dtype: torch.dtype,
+):
+    """
+    Apply data parallelism to the model. FSDP2 is used here.
+    """
+    mp_policy = MixedPrecisionPolicy(param_dtype=param_dtype, reduce_dtype=reduce_dtype)
+    fsdp_config = {"mesh": dp_mesh, "mp_policy": mp_policy}
+
+    for layer_id, transformer_block in model.layers.items():
+
+        # As an optimization, do not reshard after forward for the last
+        # transformer block since FSDP would prefetch it immediately
+        reshard_after_forward = int(layer_id) < len(model.layers) - 1
+        fully_shard(
+            transformer_block,
+            **fsdp_config,
+            reshard_after_forward=reshard_after_forward,
         )
-
-        if parallel_dims.dp_replicate_enabled:
-            logger.info("Applied HSDP to the model")
-        else:
-            logger.info("Applied FSDP to the model")
-
-        if parallel_dims.cp_enabled:
-            logger.info("Applied Context Parallel to the model")
-    elif parallel_dims.dp_replicate_enabled:
-        if world_mesh.ndim > 1:
-            raise RuntimeError("DDP has not supported > 1D parallelism")
-        apply_ddp(
-            model,
-            world_mesh,
-            enable_compile=job_config.training.compile,
-            enable_compiled_autograd=job_config.experimental.enable_compiled_autograd,
-        )
+    fully_shard(model, **fsdp_config, reshard_after_forward=True)
